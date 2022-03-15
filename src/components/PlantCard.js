43e1889f16e1cd1cf8468f9f7d0ce54f503591cc
Candice Smith MacKenzie <Mahony8@aol.com>
2022-03-14 23:09:54 -0400

    completed assignment

--- src/components/PlantCard.js
+++ src/components/PlantCard.js
@@ -1,16 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 
-function PlantCard() {
+function PlantCard({plant:{id, name, image, price}, onDeletePlant }) {
+  const [inStock, setInStock] = useState(true)
+
+const handleStockChange = () => {
+  setInStock(!inStock)
+}
+
+ function handleDelete(){
+  fetch(`http://localhost:6001/plants/${id}`, {
+      method: "DELETE"
+  })
+    onDeletePlant(id)
+}
+
   return (
     <li className="card">
-      <img src={"https://via.placeholder.com/400"} alt={"plant name"} />
-      <h4>{"plant name"}</h4>
-      <p>Price: {"plant price"}</p>
-      {true ? (
-        <button className="primary">In Stock</button>
+      <img src={image} alt={name} />
+      <h4>{name}</h4>
+      <p>Price: ${price}</p>
+      {inStock ? (
+        <button 
+        onClick={handleStockChange}
+        className="primary">In Stock</button>
       ) : (
-        <button>Out of Stock</button>
+        <button
+        onClick={handleStockChange}
+        >Out of Stock</button>
       )}
+      <button onClick={handleDelete}>Delete</button>
     </li>
   );
 }
--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,14 +1,37 @@
-import React from "react";
+import React, { useState, useEffect }from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+  const [plants, setPlants] = useState ([])
+  const [search, setSearch] = useState("")
+
+  useEffect(() => {
+    fetch(" http://localhost:6001/plants")
+    .then(resp => resp.json())
+    .then(plants => setPlants(plants) 
+    )
+  }, [])
+
+  const addPlant = (newPlant) => {
+    const updatedPlants = [...plants, newPlant]
+    setPlants(updatedPlants)
+ }
+  const deletePlant = (id) => {
+    const updatedPlants = plants.filter(plant => plant.id !== id)
+    setPlants(updatedPlants)
+}
+ 
+  const displayedPlants = plants.filter((plant) => {
+    return plant.name.toLowerCase().includes(search.toLowerCase())
+   })
+
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm onAddPlant={addPlant}/>
+      <Search search={search} onSearchChange={setSearch}/>
+      <PlantList plants={displayedPlants} onDeletePlant={deletePlant}/>
     </main>
   );
 }
